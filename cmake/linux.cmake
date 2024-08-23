4bf1d737bd7155de1ddb3fbd2e6ff9cd91aa9125
yanagiis <yanagiis@gmail.com>
2024-08-23 12:14:25 +0800

    feat: add zephyr support

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -1,8 +1,32 @@
-set(SRCS ${BORING_JSON_ROOT_DIR}/src/boring_json_encode.c
-         ${BORING_JSON_ROOT_DIR}/src/boring_json_decode.c)
+option(CONFIG_DEBUG "Enable debug print" OFF)
+option(CONFIG_TESTING "Build testing" OFF)
 
-add_library(boring_json ${SRCS})
-target_include_directories(boring_json PUBLIC ${BORING_JSON_ROOT_DIR}/include)
+add_library(boring_json ${SOURCES})
 
-add_executable(test_prim ${BORING_JSON_ROOT_DIR}/test/prim.c)
-target_link_libraries(test_prim boring_json)
+target_include_directories(
+  boring_json
+  PUBLIC ${BORING_JSON_ROOT_DIR}/include
+  PRIVATE ${BORING_JSON_ROOT_DIR}/src)
+
+if(CONFIG_BORING_JSON_TESTING)
+
+  # deps/unity
+
+  set(UNITY_SRCS ${BORING_JSON_ROOT_DIR}/deps/unity/src/unity.c)
+  add_library(unity ${UNITY_SRCS})
+  target_include_directories(unity
+                             PUBLIC ${BORING_JSON_ROOT_DIR}/deps/unity/src)
+  target_compile_options(unity PUBLIC "-DUNITY_INCLUDE_DOUBLE")
+
+  # test
+
+  set(TEST_SRCS
+      ${BORING_JSON_ROOT_DIR}/test/test_decode.c
+      ${BORING_JSON_ROOT_DIR}/test/test_encode.c
+      ${BORING_JSON_ROOT_DIR}/test/runner.c)
+
+  add_executable(boring_json_test ${TEST_SRCS})
+  target_include_directories(boring_json_test PRIVATE deps/unity/src)
+  target_link_libraries(boring_json_test boring_json unity)
+
+endif()
---
+++ cmake/zephyr.cmake
@@ -0,0 +1,5 @@
+zephyr_library()
+
+zephyr_library_sources(${SOURCES})
+
+zephyr_include_directories(${BORING_JSON_ROOT_DIR}/include/)
